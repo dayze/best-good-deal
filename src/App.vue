0cf1dec992c4f98687fd65ac151166753ea93608
Dez <elye24@gmail.com>
2017-11-07 14:26:56 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-ons-page id="app">
+    <v-ons-splitter>
+      <v-ons-splitter-side swipeable collapse width="250px"
+                           :animation="$ons.platform.isAndroid() ? 'overlay' : 'reveal'"
+                           :open.sync="menuIsOpen">
+        <menu-page></menu-page>
+      </v-ons-splitter-side>
+
+      <v-ons-splitter-content>
+        <home-page></home-page>
+      </v-ons-splitter-content>
+    </v-ons-splitter>
+  </v-ons-page>
+</template>
+
+<script>
+  import Vue from 'vue'
+  import HomePage from './components/HomePage'
+  import MenuPage from './components/MenuPage'
+
+  export default {
+    name: 'app',
+    mounted () {
+      Vue.cordova.on('deviceready', () => {
+        /* eslint-disable no-undef */
+        FCMPlugin.onNotification(function (data) {
+          if (data.wasTapped) {
+            alert(JSON.stringify(data))
+          } else {
+            alert(JSON.stringify(data))
+          }
+        })
+      })
+    },
+    data () {
+      return {
+        cordova: Vue.cordova
+      }
+    },
+    computed: {
+      menuIsOpen: {
+        get () {
+          return this.$store.state.splitter.open
+        },
+        set (newValue) {
+          this.$store.commit('splitter/toggle', newValue)
+        }
+      }
+    },
+    components: {
+      HomePage,
+      MenuPage
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ons-splitter-side[side=left][animation=overlay] {
+    border-right: 1px solid #BBB;
+  }
+</style>
